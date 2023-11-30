53fead1d394abcfd12b52f66811e4467243c7d83
guilealp <guilherme.3110@hotmail.com>
2023-11-30 09:49:01 -0300

    aula 30/11

--- src/componets/Cadastro.tsx
+++ src/componets/Cadastro.tsx
@@ -10,8 +10,17 @@ const Cadastro = () => {
     const[email, setEmail] = useState<string>("");
     const[cpf, setCpf] = useState<string>("");
     const[password, setPassword] = useState<string>("");
+    const[nomeErro, setNomeErro] = useState<string>("");
+    const[emailErro, setEmailErro]=useState<string>("");
+    const[cpfErro,setCpfErro]=useState<string>("");
+    const[passwordErro,setPasswordErro]=useState("");
 
     const cadastrarUsuario = (e: FormEvent) => {
+        setNomeErro("")
+        setEmailErro("")
+        setCpfErro("")
+        setPasswordErro("")
+
         e.preventDefault();
         const dados={
             nome:nome,
@@ -19,17 +28,34 @@ const Cadastro = () => {
             cpf:cpf,
             password:password
         }   
-    axios.post('http://10.137.9.134:8000/api/store',dados,{
+    axios.post('http://10.137.9.136:8000/api/store',dados,{
         headers:{
             "Accept":"application/json",
             "Content-Type":"application/json"
         }
     }).then(function(response){
+        if(response.data.success === false){
+            if('nome' in response.data.error){
+                setNomeErro(response.data.error.nome[0])
+            }
+            if('email' in response.data.error){
+                setEmailErro(response.data.error.email[0])
+            }
+            if('cpf' in response.data.error){
+                setCpfErro(response.data.error.cpf[0])
+            }
+            if('password' in response.data.error){
+                setPasswordErro(response.data.error.password[0])
+            }
+        }
+        else{
         window.location.href="/listagem";
+    }
     }).catch(function(error){
         console.log(error);
         
-    }); }
+    }); 
+}
 
     const handleState = (e: ChangeEvent<HTMLInputElement>) =>{
         if(e.target.name=== "nome"){
@@ -58,19 +84,23 @@ const Cadastro = () => {
                             <form onSubmit={cadastrarUsuario} className='row g-3'>
                             <div className='col-6'>
                                 <label htmlFor="nome" className='form-label'>nome</label>
-                                <input type="text" name='nome' className='form-control' required  onChange={handleState}/>
+                                <input type="text" name='nome' className='form-control' required  onChange={handleState} />
+                                <div className='text-danger'>{nomeErro}</div>
                             </div>
                             <div className='col-6'>
                             <label htmlFor="email" className='form-label'>email</label>
                                 <input type="text" name='email' className='form-control' required onChange={handleState}/>
+                                <div className='text-danger'>{emailErro}</div>
                             </div>
                             <div className='col-6'>
                             <label htmlFor="cpf" className='form-label'>cpf</label>
                                 <input type="text" name='cpf' className='form-control' required onChange={handleState}/>
+                                <div className='text-danger'>{cpfErro}</div>
                             </div>
                             <div className='col-6'>
                             <label htmlFor="password" className='form-label'>senha</label>
                                 <input type="password" name='password' className='form-control' required onChange={handleState}/>
+                                <div className='text-danger'>{passwordErro}</div>
                             </div>
                             <div className='col-12'>
                                 <button type='submit' className='btn btn-success bt-sm'>Cadastrar</button>
